5183be06e69179f40588ade23321f7ece88e3b09
Patrick Lam <prof.lam@gmail.com>
2009-08-17 11:15:55 -0400

    Remove camelcase since we can use _ in XML nodenames.

--- _constants.php
+++ _constants.php
@@ -5,14 +5,4 @@ $ALL_FIELDS = $GLOBAL_FIELDS;
 array_push($ALL_FIELDS, "version");
 $GRADES_FIELDS = array("date_grade", "grade");
 $SERVICE_FIELDS = array("date_inscription", "cours", "sessions", "passeport", "non_anjou", "judogi", "escompte", "frais", "cas_special_note", "horaire_special");
-
-function camelCase($s) {
- $p = '/\S[A-Z]/';
- $p1 = '/(.)([A-Z])/';
- $p2 = '/(_)([a-z])/';
- if (preg_match($p, $s))
-  return (preg_replace_callback($p1, create_function('$matches', 'return $matches[1] . "_" . strtolower($matches[2]);'), $s));
- else
-  return (preg_replace_callback($p2, create_function('$matches', 'return strtoupper($matches[2]);'), $s));
-}
 ?>
