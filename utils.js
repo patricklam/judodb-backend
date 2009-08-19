993aa1390364a3f0719d5fdd7c634ea9ea6ef25e
Patrick Lam <prof.lam@gmail.com>
2009-08-18 21:24:39 -0400

    Fix layout, make XHTML 1.0 Transitional.

--- utils.js
+++ utils.js
@@ -126,3 +126,16 @@ function formatDate(date1) {
     (date1.getMonth() < 9 ? '0' : '') + (date1.getMonth()+1) + '-' +
     (date1.getDate() < 10 ? '0' : '') + date1.getDate();
 }
+
+// Courtesy http://www.netlobo.com/url_query_string_javascript.html
+function gup( name )
+{
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return null;
+  else
+    return results[1];
+}
