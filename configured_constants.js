9e553b8a6d085e32dac4e10ce0c30a5ab990131c
Patrick Lam <prof.lam@gmail.com>
2009-10-03 18:11:18 -0400

    Use config info from local database.

---
+++ configured_constants.js
@@ -0,0 +1,87 @@
+var db;
+var store = new DataStore();
+store.init();
+
+var CURRENT_SESSION; 
+var CURRENT_SESSION_YEAR;
+var CURRENT_SESSION_SEQNO;
+var CURRENT_SESSION_FIRST_SIGNUP;
+var NEXT_SESSION;
+
+var COURS = [], COURS_SHORT = [], COURS_ENTRAINEURS = [];
+var CATEGORY_NAMES = [], CATEGORY_YEARS = [], CATEGORY_ABBREVS = [],
+    CATEGORY_NOIRE = [];
+
+initConfig();
+function initConfig() {
+    initSession();
+    initCours();
+    initCategorie();
+}
+
+function initSession() {
+    var rs = db.execute("SELECT * FROM `session` WHERE "
+			+ "session.first_signup_date < date() AND "
+			+ "session.last_signup_date > date()");
+    if (!rs.isValidRow()) {
+	setError('Aucun session en cours! Veuillez modifier la configuration.');
+	return;
+    }
+    CURRENT_SESSION = rs.fieldByName('abbrev');
+    CURRENT_SESSION_YEAR = parseInt(rs.fieldByName('year'));
+    CURRENT_SESSION_SEQNO = rs.fieldByName('seqno');
+    CURRENT_SESSION_FIRST_SIGNUP = rs.fieldByName('first_signup_date');
+    rs.close();
+
+    // next session
+    rs = db.execute("SELECT abbrev FROM `session` WHERE "
+		    + "session.seqno = ?", [CURRENT_SESSION_SEQNO + 1]);
+    if (rs.isValidRow())
+	NEXT_SESSION = rs.field(0);
+    rs.close();
+}
+
+function initCours() {
+    var rs = db.execute("SELECT * FROM `cours`, `cours_session` WHERE "
+			+ "cours.seqno = cours_session.cours_seqno AND "
+			+ "cours_session.session_seqno = ? "
+			+ "ORDER BY seqno", 
+		       [CURRENT_SESSION_SEQNO]);
+    while (rs.isValidRow()) {
+	COURS = COURS.concat(rs.fieldByName('name'));
+	COURS_SHORT = COURS_SHORT.concat(rs.fieldByName('short_desc'));
+	COURS_ENTRAINEURS = COURS_ENTRAINEURS.concat(rs.fieldByName('entraineur'));
+	rs.next();
+    }
+    rs.close();
+}
+
+function initCategorie() {
+    var rs = db.execute("SELECT * FROM `categorie`");    
+    while (rs.isValidRow()) {
+	CATEGORY_NAMES = CATEGORY_NAMES.concat(rs.fieldByName('name'));
+	CATEGORY_ABBREVS = CATEGORY_ABBREVS.concat(rs.fieldByName('abbrev'));
+	CATEGORY_NOIRE = CATEGORY_NOIRE.concat(rs.fieldByName('noire'));
+
+	var sy = 0;
+	if (rs.fieldByName('years_ago') != '')
+	    sy = CURRENT_SESSION_YEAR - rs.fieldByName('years_ago') + 2;
+
+	CATEGORY_YEARS = CATEGORY_YEARS.concat(sy);
+	rs.next();
+    }
+    rs.close();
+}
+
+// not currently in db.
+var SUGGESTED_PAIEMENTS = ["", "2009-11-13", "2010-02-05", "", "", ""];
+
+var FRAIS_PASSEPORT_JUDO_QUEBEC = 5;
+var FRAIS_PAS_ANJOU = 5;
+
+var CATEGORY_PRIX_1_SESSION = [100, 100, 115, 150, 185, 185, 185, 210, 140, 140];
+var CATEGORY_PRIX_2_SESSION = [175, 175, 195, 225, 290, 290, 300, 345, 140, 140];
+var CATEGORY_PRIX_JUDO_QC = [10, 15, 25, 35, 45, 50, 60, 65, 90, 100];
+
+var ESCOMPTE_NAMES = ["Aucun", "2e membre", "3e membre", "4e membre", "Nouvel(le) ami(e)", "Membre du CA", "Cas spécial"];
+var ESCOMPTE_AMOUNTS = [0, 10, 15, 20, 10, 50, -1];
--- constants.js
+++ constants.js
@@ -31,82 +31,10 @@ var SESSION_FIELDS = ['id', 'seqno', 'name', 'year', 'abbrev',
 		     'first_class_date', 'first_signup_date',
 		     'last_class_date', 'last_signup_date'];
 var COURS_FIELDS = ['id', 'seqno', 'name', 'short_desc', 'entraineur'];
+var CATEGORIES_FIELDS = ['id', 'name', 'abbrev', 'years_ago', 'noire'];
 
 var STORE_NAME = "anjoudb";
 var MAX_VERSEMENTS = 6;
 var MAX_GRADES = 20;
 
-/* All this stuff below should eventually end up in the database. */
-
-var SAISON = 2009;
-var CURRENT_SESSION="A09"; var NEXT_SESSION="H10";
-var CATEGORY_NAMES = ["Mini-Poussin (U-7)", "Poussin (U-9)", "Benjamin (U-11)",
-                      "Minime (U-13)", "Juvénile (U-15)", "Cadet (U-17)",
-                      "Junior (U-20)", "Sénior", "Junior Noire", "Sénior Noire"];
-var CATEGORY_YEARS = [SAISON-5, SAISON-7, SAISON-9, SAISON-11, SAISON-13,
-                      SAISON-15, SAISON-18, 0];
-
-var COURS = ["Adultes (LM2015-2145, V2000-2145)", 
-             "Équipe compétition (LM1830-2015, V2000-2145)",
-             "Intérmediares 7-12 ans (L1730-1830, V1830-2000)",
-             "Débutants 7-12 ans (MaJ1730-1830)",
-             "Débutants de 5 à 6 ans (S900-1000)",
-             "Anciens de 5 à 6 ans (S1000-1100)",
-             "Anciens de 7 à 8 ans (S1100-1230)",
-             "Anciens de 7 à 9 ans (S1230-1400)",
-             "Débutants de 7 à 9 ans (S1400-1530)",
-             "Débutants de 7 à 8 ans (S1530-1700)",
-             "Débutants de 5 à 6 ans (D900-1000)",
-             "Débutants de 7 à 10 ans (D1030-1230)",
-             "Débutants de 7 à 11 ans (MV1730-1830)",
-             "Autre"];
-
-var COURS_SHORT = ["LM2015 V2000", 
-             "LM1830 V2000",
-             "L1730 V1830",
-             "MaJ1730",
-             "S900",
-             "S1000",
-             "S1100",
-             "S1230",
-             "S1400",
-             "S1530",
-             "D900",
-             "D1030",
-             "MV1730",
-             "?"];
-
-var COURS_ENTRAINEURS = ["Rejean Lavoie", 
-			 "Jean-François Marceau", 
-			 "Rejean Lavoie",
-			 "Pierre Chagnon",
-			 "Luc Phan",
-			 "Luc Phan",
-			 "Luc Phan",
-			 "Luc Phan",
-			 "Luc Phan",
-			 "Luc Phan",
-			 "Lan-Anh Phan",
-			 "Lan-Anh Phan",
-			 "Rejean Lavoie",
- 			 ""];
-
-var SUGGESTED_PAIEMENTS = ["", "2009-11-13", "2010-02-05", "", "", ""];
-
-var FRAIS_PASSEPORT_JUDO_QUEBEC = 5;
-var FRAIS_PAS_ANJOU = 5;
-
-var CATEGORY_PRIX_1_SESSION = [100, 100, 115, 150, 185, 185, 185, 210, 140, 140];
-var CATEGORY_PRIX_2_SESSION = [175, 175, 195, 225, 290, 290, 300, 345, 140, 140];
-var CATEGORY_JUDO_QC = [10, 15, 25, 35, 45, 50, 60, 65, 90, 100];
-
-var ESCOMPTE_NAMES = ["Aucun", "2e membre", "3e membre", "4e membre", "Nouvel(le) ami(e)", "Membre du CA", "Cas spécial"];
-var ESCOMPTE_AMOUNTS = [0, 10, 15, 20, 10, 50, -1];
-
-function categoryName(i) { return CATEGORY_NAMES[i]; }
-function categoryPrix1(i) { return CATEGORY_PRIX_1_SESSION[i]; }
-function categoryPrix2(i) { return CATEGORY_PRIX_2_SESSION[i]; }
-function categoryPrixJQ(i) { return CATEGORY_JUDO_QC[i]; }
-
-var FIRST_2009_INSCRIPTION = '2009-08-28';
 
