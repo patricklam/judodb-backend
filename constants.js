fa205d5fb9ad48765451b7c269eedc2a1ed2cbe3
Patrick Lam <prof.lam@gmail.com>
2010-09-13 23:58:27 -0400

    Bump version.

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-var VERSION = '100822-beta4';
+var VERSION = '100913-beta4';
 
 // See also constants.php for another definition of GLOBAL_FIELDS.
 var GLOBAL_FIELDS = ["nom", "prenom", "ddn", "courriel", "adresse", "ville", "code_postal", "tel", "affiliation", "carte_anjou", "nom_recu_impot", "nom_contact_urgence", "tel_contact_urgence", "sexe", "nom_stripped", "prenom_stripped"];
