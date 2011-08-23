41a2aedaf975f5ea9bb21ee3b9078595159ebb85
Patrick Lam <prof.lam@gmail.com>
2011-08-23 16:51:25 -0400

    Fix logout script.

--- logout.php
+++ logout.php
@@ -1,5 +1,19 @@
 <?php
 session_start();
+$_SESSION[authenticated] = "";
+$_SESSION[username] = "";
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+   );
+}
+
 session_destroy();
+header('content-type: application/json');
+$callback = trim($_GET['callback']);
+echo $callback . '({"authenticated":"EXPIRED"})';
+
 ?>
-Logged out.
