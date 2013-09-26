9743e85c99258ec580fb055bd65e87e04f5e1143
Patrick Lam <prof.lam@gmail.com>
2013-09-26 13:17:59 -0400

    use 2013 ft-303

--- listesft.php
+++ listesft.php
@@ -8,7 +8,7 @@ require ('produceoutput.php');
 
 $pdf =& new FPDI('P', 'mm', 'Letter');
 
-$pagecount = $pdf->setSourceFile('ft303.pdf'); 
+$pagecount = $pdf->setSourceFile('ft303version2013.pdf'); 
 $tplidx = $pdf->importPage(1, '/MediaBox'); 
 $pdf->SetAutoPageBreak(false);
 
@@ -27,13 +27,13 @@ $clubno = $c[1];
 $COLS = 14;
 $display = array(true, true, false, true, true, false, false, false, false, false, false, false, false, true);
 $x = array(128, 128, 0, 178, 51, 0, 0, 0, 0, 0, 0, 0, 0, 128);
-$y = array(20, 27.5, 0, 42, 57, 0, 0, 0, 0, 0, 0, 0, 0, 42);
-$INCREMENT = 81.5;
+$y = array(20, 27, 0, 41, 56.5, 0, 0, 0, 0, 0, 0, 0, 0, 41);
+$INCREMENT = 84;
 
 $catX = array('M' => 52, 'S' => 52, 'U21' => 71.5, 'U18' => 52, 
-      'U15' => 71.5, 'U13' => 52, 'U11' => 71.5, 'U9' => 52, 'U7' => 71.5);
+      'U16' => 71.5, 'U14' => 52, 'U12' => 71.5, 'U10' => 52, 'U8' => 71.5);
 $catY = array('M' => 21, 'S' => 27, 'U21' => 27, 'U18' => 33.5, 
-      'U15' => 33.5, 'U13' => 41, 'U11' => 41, 'U9' => 48, 'U7' => 48);
+      'U16' => 33.5, 'U14' => 41, 'U12' => 41, 'U10' => 47, 'U8' => 47);
 
 $actualCount = 0;
 $data = iconv("UTF-8", "ISO-8859-1", $_POST['data_full']);
@@ -41,7 +41,6 @@ $ds = explode("*", $data);
 $allCount = count($ds);
 for ($i = 0; $i < $allCount; $i++) {
     if ($ds[$i] == '') continue;
-
     if (($i % 3 == 0) && ($i > 0)) {
        $pdf->addPage(); 
        $pdf->useTemplate($tplidx); 
@@ -53,18 +52,18 @@ for ($i = 0; $i < $allCount; $i++) {
     $d[4] = substr($d[4], 0, 4);
     $date = str_replace("/", "     ", 
             str_replace("-", "     ", $_POST['date']));
-    $pdf->SetXY(128, 34.5 + $effOff);
+    $pdf->SetXY(128, 33.5 + $effOff);
     $pdf->Cell(0, 0, $club);
-    $pdf->SetXY(175, 34.5 + $effOff);
+    $pdf->SetXY(175, 33.5 + $effOff);
     $pdf->Cell(0, 0, $clubno);
-    $pdf->SetXY(133, 73 + $effOff);
+    $pdf->SetXY(133, 72.5 + $effOff);
     $pdf->Cell(0, 0, $_POST['evt']);
     $pdf->SetXY(48, 65 + $effOff);
     $pdf->Cell(0, 0, $date);
 
     // division
     if (substr($d[5], -1) == 'N') $d[5] = substr($d[5], 0, -1);
-    if ($d[16] == 'M') $d[5] = 'M';
+    if ($d[17] == 'M') $d[5] = 'M';
     $pdf->SetXY($catX[$d[5]], $catY[$d[5]] + $effOff);
     $pdf->Cell(0, 0, 'X');
 
@@ -74,7 +73,7 @@ for ($i = 0; $i < $allCount; $i++) {
     if ($d[2] == 'F')
       $sx = 46.2;
     if ($sx > 0) {
-      $pdf->SetXY(60 + $sx, 57 + $effOff);
+      $pdf->SetXY(60 + $sx, 55.7 + $effOff);
       $pdf->Cell(0, 0, "X");
     }
 
