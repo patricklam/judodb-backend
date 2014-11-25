14dfb9520a57c167356d113f359822c235fbf0cf
Patrick Lam <prof.lam@gmail.com>
2014-11-25 22:13:57 +0000

    Send all cours information to the client

--- pull_club_cours.php
+++ pull_club_cours.php
@@ -10,15 +10,15 @@ require ('_dbconfig.php');
 $link = mysql_connect($DBI_HOST, $DBI_USERNAME, $DBI_PASSWORD) || die("could not connect to db");
 mysql_select_db($DBI_DATABASE) || die("could not select db");
 
-$club_id = $_GET["club_id"];
-if (!isset ($club_id)) die;
+$numero_club = $_GET["club_id"];
+if (!isset ($numero_club)) die;
 
 $courslist = array();
-$rs0 = mysql_query("SELECT * FROM `club_cours` WHERE club_id=$club_id");
+$rs0 = mysql_query("SELECT `club_cours`.* FROM `club_cours` JOIN `club` ON `club_cours`.club_id=`club`.club_id WHERE numero_club='$numero_club'");
 
 if(isset($rs0)) {
   while ($club_cours = mysql_fetch_object($rs0)) {
-    $courslist[] = $club_cours->seq_no;
+    $courslist[] = $club_cours;
   } 
 }
 
