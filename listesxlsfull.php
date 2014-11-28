1f5dab1363d09d79711aa53f97cb4a90b5c3ce9b
Patrick Lam <prof.lam@gmail.com>
2014-11-28 06:03:40 +0000

    remove misleading comments

--- listesxlsfull.php
+++ listesxlsfull.php
@@ -7,8 +7,7 @@ require ('PHPExcel/PHPExcel/IOFactory.php');
 
 $objPHPExcel = new PHPExcel();
 
-// to avoid the need for syncronisation before output, use POST params 
-// for the data in the list.
+// to avoid need for backend smarts, use POST params for the data in the list.
 
 $data = $_POST['data_full'];
 $ds = explode("*", $data);
