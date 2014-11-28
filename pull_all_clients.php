3282b98f26102213f9757ee30f849f86d4c04468
Patrick Lam <prof.lam@gmail.com>
2014-11-28 03:13:08 +0000

    Use PDO for pull_all_clients and pull_one_client.

--- pull_all_clients.php
+++ pull_all_clients.php
@@ -1,4 +1,5 @@
 <?
+require ('_pdo.php');
 require ('_authutils.php');
 
 require_authentication();
@@ -6,34 +7,29 @@ require_authentication();
 ini_set('zlib.output_compression', 4096);
 header('content-type: application/json');
 
-require ('_dbconfig.php');
+$db = pdo_db_connect();
+$gquery = $db->prepare('SELECT * FROM `grades` ' .
+                       'WHERE client_id=:client_id ORDER BY date_grade ASC');
+$squery = $db->prepare('SELECT * FROM `services` ' .
+                       'WHERE client_id=:client_id ORDER BY date_inscription ASC');
 
-$link = mysql_connect($DBI_HOST, $DBI_USERNAME, $DBI_PASSWORD) || die("could not connect to db");
-mysql_select_db($DBI_DATABASE) || die("could not select db");
-
-$rs0 = mysql_query("SELECT * FROM `client`");
 $clients = array();
-while ($client = mysql_fetch_object($rs0)) {
+foreach ($db->query('SELECT * FROM `client`',PDO::FETCH_OBJ) as $client) {
   $id = $client->id;
-  $rs = mysql_query("SELECT * FROM `grades` " .
-                    "WHERE client_id=$id ORDER BY date_grade ASC");
-  if (isset($rs)) {
-   $client->grades = array();
-   while ($g = mysql_fetch_object($rs)) {
+  $params = array(':client_id' => $id);
+  $gquery->execute($params);
+  $client->grades = array();
+  foreach ($gquery->fetchAll(PDO::FETCH_OBJ) as $g) {
     unset($g->client_id);
     unset($g->id);
     $client->grades[] = $g;
-   }
   }
 
-  $rs = mysql_query("SELECT * FROM `services` " .
-                   "WHERE client_id=$id ORDER BY date_inscription ASC");
-  if (isset($rs)) {
-   $client->services = array();
-   while ($s = mysql_fetch_object($rs)) {
+  $client->services = array();
+  $squery->execute($params);
+  foreach ($squery->fetchAll(PDO::FETCH_OBJ) as $s) {
     unset($s->client_id);
     $client->services[] = $s;
-   }
   }
   $clients[] = $client;
 }
