bf68f395434d6f6004a5a2fad64475a8af7add18
Patrick Lam <prof.lam@gmail.com>
2014-11-28 01:24:13 +0000

    Start migration to PDO.

--- pull_all_users.php
+++ pull_all_users.php
@@ -1,24 +1,21 @@
 <?
 require ('_constants.php');
-require ('_database.php');
+require ('_pdo.php');
 require ('_authutils.php');
 require ('_userutils.php');
 
 require_authentication();
 
-db_connect() || die;
+$db = pdo_db_connect();
 
-$rs = db_query_get("SELECT * FROM `user`");
 $users = array();
-if (isset($rs)) {
- foreach ($rs as $r) {
+foreach ($db->query('SELECT * FROM `user`') as $r) {
   $user = new stdClass();
   $user->id = $r['id'];
   $user->username = $r['username'];
   if (is_admin()) $user->email = $r['email'];
   $user->last_update = $r['last_update'];
   $users[] = $user;
- }
 }
 
 $callback = trim($_GET['callback']);
