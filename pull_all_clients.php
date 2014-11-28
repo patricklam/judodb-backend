5ec00ff7a466f6e2a588648222c343df9c4bf3fe
Patrick Lam <prof.lam@gmail.com>
2014-11-28 05:57:04 +0000

    add access control for all_clients list

--- pull_all_clients.php
+++ pull_all_clients.php
@@ -3,12 +3,13 @@ require_once ('_pdo.php');
 require_once ('_authutils.php');
 require_once ('_userutils.php');
 
-require_authentication();
-
 ini_set('zlib.output_compression', 4096);
 header('content-type: application/json');
 
 $db = pdo_db_connect();
+require_authentication($db);
+$user_id = get_user_id($db);
+
 $gquery = $db->prepare('SELECT * FROM `grades` ' .
                        'WHERE client_id=:client_id ORDER BY date_grade ASC');
 $squery = $db->prepare('SELECT * FROM `services` ' .
@@ -18,6 +19,16 @@ $clients = array();
 foreach ($db->query('SELECT * FROM `client`',PDO::FETCH_OBJ) as $client) {
   $id = $client->id;
   $params = array(':client_id' => $id);
+
+  $client->services = array();
+  $squery->execute($params);
+  $visible = false;
+  foreach ($squery->fetchAll(PDO::FETCH_OBJ) as $s) {
+    unset($s->client_id);
+    $client->services[] = $s;
+    if (can_access_club($db, $user_id, $s->club_id)) $visible = true;
+  }
+
   $gquery->execute($params);
   $client->grades = array();
   foreach ($gquery->fetchAll(PDO::FETCH_OBJ) as $g) {
@@ -26,13 +37,8 @@ foreach ($db->query('SELECT * FROM `client`',PDO::FETCH_OBJ) as $client) {
     $client->grades[] = $g;
   }
 
-  $client->services = array();
-  $squery->execute($params);
-  foreach ($squery->fetchAll(PDO::FETCH_OBJ) as $s) {
-    unset($s->client_id);
-    $client->services[] = $s;
-  }
-  $clients[] = $client;
+  if ($visible)
+    $clients[] = $client;
 }
 
 $callback = trim($_GET['callback']);
