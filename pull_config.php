662506aaac9b120e28daa1df3323c6139edb74a9
Patrick Lam <prof.lam@gmail.com>
2014-11-28 03:14:04 +0000

    Fix to vacuous file.

--- pull_config.php
+++ pull_config.php
@@ -1,19 +1,17 @@
 <?
+require ('_pdo.php');
 require ('_authutils.php');
 
 require_authentication();
 
 header('content-type: application/json');
 
-require ('_dbconfig.php');
-
-$link = mysql_connect($DBI_HOST, $DBI_USERNAME, $DBI_PASSWORD) || die("could not connect to db");
-mysql_select_db($DBI_DATABASE) || die("could not select db");
+$db = pdo_db_connect();
 
 $callback = trim($_GET['callback']);
 echo $callback;
 echo '(';
-//echo json_encode($client);
+// TODO
 echo ');';
 
 ?>
