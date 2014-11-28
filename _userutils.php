db564749c2533e8505ab8b167393adb1ff29e50b
Patrick Lam <prof.lam@gmail.com>
2014-11-28 04:18:35 +0000

    remove unused functions

--- _userutils.php
+++ _userutils.php
@@ -47,48 +47,4 @@ function get_club_list($db) {
   }
   return $clubs;
 }
-
-function utf8_encode_deep(&$input) {
-    if (is_string($input)) {
-        $input = utf8_encode($input);
-    } else if (is_array($input)) {
-        foreach ($input as &$value) {
-            utf8_encode_deep($value);
-        }
-
-        //unset($value);
-    } else if (is_object($input)) {
-        $vars = array_keys(get_object_vars($input));
-
-        foreach ($vars as $var) {
-            utf8_encode_deep($input->$var);
-        }
-    }
-}
-
-function check_json_error() {
-  switch (json_last_error()) {
-  	case JSON_ERROR_NONE:
-            echo ' - No errors';
-        break;
-        case JSON_ERROR_DEPTH:
-            echo ' - Maximum stack depth exceeded';
-        break;
-        case JSON_ERROR_STATE_MISMATCH:
-            echo ' - Underflow or the modes mismatch';
-        break;
-        case JSON_ERROR_CTRL_CHAR:
-            echo ' - Unexpected control character found';
-        break;
-        case JSON_ERROR_SYNTAX:
-            echo ' - Syntax error, malformed JSON';
-        break;
-        case JSON_ERROR_UTF8:
-            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-        break;
-        default:
-            echo ' - Unknown error';
-	    break;
-  }
-}
 ?>
