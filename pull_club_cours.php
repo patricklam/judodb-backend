b66f26ead3331743523ad86acd6baf002ca24b2c
Patrick Lam <prof.lam@gmail.com>
2014-11-29 05:37:49 +0000

    generalize pull_club_cours API to pull multiple clubs' cours.

--- pull_club_cours.php
+++ pull_club_cours.php
@@ -10,19 +10,29 @@ require_authentication($db);
 
 $numero_club = $_GET["numero_club"];
 $session_seqno = $_GET["session_seqno"];
-if (!isset ($numero_club) || !isset($session_seqno)) die;
+if (!isset($session_seqno)) die;
 
-$club_query = $db->prepare('SELECT `id` FROM `club` WHERE `numero_club`=?');
-$club_query->execute(array($numero_club));
-if ($club_query->rowCount() == 0) die;
-$club_id = $club_query->fetch(PDO::FETCH_OBJ)->id;
-if (!can_access_club($db, get_user_id($db), $club_id)) die;
+if (!isset($numero_club)) {
+  $club_query = $db->prepare('SELECT `id` FROM `club`');
+  $club_query->execute();
+} else {
+  $club_query = $db->prepare('SELECT `id` FROM `club` WHERE `numero_club`=?');
+  $club_query->execute(array($numero_club));
+}
 
-$courslist = array();
-$cc_query = $db->prepare('SELECT * FROM `club_cours` WHERE `club_id`=? AND `session_seqno`=?');
-$cc_query->execute(array($club_id, $session_seqno));
-foreach ($cc_query->fetchAll(PDO::FETCH_OBJ) as $club_cours) {
-  $courslist[] = $club_cours;
+if ($club_query->rowCount() > 0) {
+  $courslist = array();
+  $cc_query = $db->prepare('SELECT * FROM `club_cours` WHERE `club_id`=? AND `session_seqno`=?');
+
+  foreach ($club_query->fetchAll(PDO::FETCH_OBJ) as $c) {
+    $club_id = $c->id;
+    if (!can_access_club($db, get_user_id($db), $club_id)) continue;
+
+    $cc_query->execute(array($club_id, $session_seqno));
+    foreach ($cc_query->fetchAll(PDO::FETCH_OBJ) as $club_cours) {
+      $courslist[] = $club_cours;
+    }
+  }
 }
 
 $callback = trim($_GET['callback']);
