5d43a817f56b1b3d02ea4a9e81e34ca3041264f4
Patrick Lam <prof.lam@gmail.com>
2014-12-09 03:18:09 +0000

    fix presences

--- listespdf.php
+++ listespdf.php
@@ -25,7 +25,6 @@ $data = iconv("UTF-8", "ISO-8859-1", $_POST['data']);
 $ds = explode("*", $data);
 
 
-// |2|Bouchard|Vincent||2D|25/11/2010|418 962-2525|12|01/01/1969|SN|S0830-0945|6|| |*
 // [blank, id, nom, prenom, sexe, grade, dategrade, tel, JudoQC, ddn, div, cours-short-desc, cours-id, ?, ?
 $display = array(false, false, true, true, false, true, false, true, false, false, false, false, false, false);
 $w = array(-1, -1, 45, 45, -1, 18, -1, 35, -1, -1, -1, -1, 70);
--- listespresences.php
+++ listespresences.php
@@ -14,21 +14,21 @@ $pdf->SetFont('Times', '', 14);
 
 $multi = $_POST["multi"];
 if ($multi == "1") {
-    $ts = explode("|", iconv("UTF-8", "ISO-8859-1", $_POST["title"]));
-    $sts = explode("|", iconv("UTF-8", "ISO-8859-1", $_POST["subtitle"]));
+    $ts = explode("|", iconv("UTF-8", "ISO-8859-1", $_POST['short_title']));
+    // $sts = explode("|", iconv("UTF-8", "ISO-8859-1", $_POST["subtitle"]));
     $c = count($ts);
 } else {
-    $ts = array(iconv("UTF-8", "ISO-8859-1", $_POST["title"]));
-    $sts = array(iconv("UTF-8", "ISO-8859-1", $_POST["subtitle"]));
+    $ts = array(iconv("UTF-8", "ISO-8859-1", $_POST['short_title']));
+    // $sts = array(iconv("UTF-8", "ISO-8859-1", $_POST["subtitle"]));
     $c = 1;
 }
 
 $data = iconv("UTF-8", "ISO-8859-1", $_POST['data']);
 $ds = explode("*", $data);
 
-    // [*cb*, "Nom", "Prenom", "Sexe", "Grade", "DateGrade", "Tel", "JudoQC", "DDN", "Cat", "Masters", "Cours", "Cours_num"];
-$display = array(false, true, true, false, false, false, true, false, false, false, false, false, false);
-$w = array(-1, 45, 45, -1, -1, -1, 30, -1, -1, -1, -1);
+// [blank, id, nom, prenom, sexe, grade, dategrade, tel, JudoQC, ddn, div, cours-short-desc, cours-id, ?, ?]
+$display = array(false, false, true, true, false, false, false, true, false, false, false, false, false, false);
+$w = array(-1, -1, 45, 45, -1, -1, -1, 30, -1, -1, -1, -1);
 produceOutput($pdf, $ts, $sts, $ds, $c, $multi, $display, $w, true);
 
 $pdf->Output();
