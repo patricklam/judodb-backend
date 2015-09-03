36d9e17208f222e3c2142fb2b89129f0260d8229
Patrick Lam <prof.lam@gmail.com>
2015-09-03 01:41:10 -0400

    also return cours from linked seqno

--- pull_club_cours.php
+++ pull_club_cours.php
@@ -9,10 +9,10 @@ $db = pdo_db_connect();
 require_authentication($db);
 
 if (isset($_GET["session_seqno"])) {
-  $ss_sql_frag = 'AND `session_seqno`=?';
+  $ss_sql_frag = 'AND (`session_seqno`=:seqno OR session_seqno=(SELECT `seqno` FROM `session` WHERE `linked_seqno`=:seqno))';
   $session_seqno = $_GET["session_seqno"];
 } else {
-  $ss_sql_frag = 'AND (`session_seqno`=? OR TRUE)';
+  $ss_sql_frag = 'AND (`session_seqno`=:seqno OR TRUE)';
   $session_seqno = '';
 }
 
@@ -26,13 +26,13 @@ if (!isset($_GET["numero_club"])) {
 
 if ($club_query->rowCount() > 0) {
   $courslist = array();
-  $cc_query = $db->prepare('SELECT * FROM `club_cours` WHERE `club_id`=? ' . $ss_sql_frag);
+  $cc_query = $db->prepare('SELECT * FROM `club_cours` WHERE `club_id`=:club_id ' . $ss_sql_frag);
 
   foreach ($club_query->fetchAll(PDO::FETCH_OBJ) as $c) {
     $club_id = $c->id;
     if (!can_access_club($db, get_user_id($db), $club_id)) continue;
 
-    $cc_query->execute(array($club_id, $session_seqno));
+    $cc_query->execute(array(':club_id' => $club_id, ':seqno' => $session_seqno));
     foreach ($cc_query->fetchAll(PDO::FETCH_OBJ) as $club_cours) {
       $courslist[] = $club_cours;
     }
