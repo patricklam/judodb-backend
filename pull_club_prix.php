04f0c13e43b8e50b7c68f58d0466129a98dda1d1
Patrick Lam <prof.lam@gmail.com>
2015-09-03 21:35:23 +0000

    allow omitting session_seqno for getting prix

--- pull_club_prix.php
+++ pull_club_prix.php
@@ -9,7 +9,13 @@ $db = pdo_db_connect();
 require_authentication($db);
 
 $numero_club = $_GET["numero_club"];
-$session_seqno = $_GET["session_seqno"];
+if (isset($_GET["session_seqno"])) {
+  $ss_sql_frag = 'AND (`session_seqno`=:seqno)';
+  $session_seqno = $_GET["session_seqno"];
+} else {
+  $ss_sql_frag = 'AND (`session_seqno`=:seqno OR TRUE)';
+  $session_seqno = '';
+}
 if (!isset ($numero_club) || !isset($session_seqno)) die;
 
 $club_query = $db->prepare('SELECT `id` FROM `club` WHERE `numero_club`=?');
@@ -19,8 +25,8 @@ $club_id = $club_query->fetch(PDO::FETCH_OBJ)->id;
 if (!can_access_club($db, get_user_id($db), $club_id)) die;
 
 $prixlist = array();
-$prix_query = $db->prepare('SELECT * FROM `club_division_session` WHERE `club_id`=? AND `session_seqno`=?');
-$prix_query->execute(array($club_id,$session_seqno));
+$prix_query = $db->prepare('SELECT * FROM `club_division_session` WHERE `club_id`=:club_id ' . $ss_sql_frag);
+$prix_query->execute(array(':club_id' => $club_id, ':seqno' => $session_seqno));
 foreach ($prix_query->fetchAll(PDO::FETCH_OBJ) as $prix) {
   unset($prix->club_id);
   unset($prix->session_seqno);
