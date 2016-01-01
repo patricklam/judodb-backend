a036b4c845d4ed53789f4dac71fbca14f216f7f6
Patrick Lam <prof.lam@gmail.com>
2016-01-01 15:31:47 +0000

    always use club_id instead of numero_club to communicate with frontend

--- pull_club_cours.php
+++ pull_club_cours.php
@@ -8,6 +8,17 @@ header('content-type: application/json');
 $db = pdo_db_connect();
 require_authentication($db);
 
+function pull_cours($db, $club_id, $session_seqno, $ss_sql_frag, $courslist) {
+  if (!can_access_club($db, get_user_id($db), $club_id)) continue;
+
+  $cc_query = $db->prepare('SELECT * FROM `club_cours` WHERE `club_id`=:club_id ' . $ss_sql_frag);
+  $cc_query->execute(array(':club_id' => $club_id, ':seqno' => $session_seqno));
+  foreach ($cc_query->fetchAll(PDO::FETCH_OBJ) as $club_cours) {
+    $courslist[] = $club_cours;
+  }
+  return $courslist;
+}
+
 if (isset($_GET["session_seqno"])) {
   $ss_sql_frag = 'AND (`session_seqno`=:seqno OR session_seqno=(SELECT `seqno` FROM `session` WHERE `linked_seqno`=:seqno))';
   $session_seqno = $_GET["session_seqno"];
@@ -16,27 +27,18 @@ if (isset($_GET["session_seqno"])) {
   $session_seqno = '';
 }
 
-if (!isset($_GET["numero_club"])) {
+$courslist = array();
+
+if (!isset($_GET["club_id"])) {
   $club_query = $db->prepare('SELECT `id` FROM `club`');
   $club_query->execute();
-} else {
-  $club_query = $db->prepare('SELECT `id` FROM `club` WHERE `numero_club`=?');
-  $club_query->execute(array($_GET["numero_club"]));
-}
-
-if ($club_query->rowCount() > 0) {
-  $courslist = array();
-  $cc_query = $db->prepare('SELECT * FROM `club_cours` WHERE `club_id`=:club_id ' . $ss_sql_frag);
 
   foreach ($club_query->fetchAll(PDO::FETCH_OBJ) as $c) {
-    $club_id = $c->id;
-    if (!can_access_club($db, get_user_id($db), $club_id)) continue;
-
-    $cc_query->execute(array(':club_id' => $club_id, ':seqno' => $session_seqno));
-    foreach ($cc_query->fetchAll(PDO::FETCH_OBJ) as $club_cours) {
-      $courslist[] = $club_cours;
-    }
+    $courslist = pull_cours($db, $c->id, $session_seqno, $ss_sql_frag, $courslist);
   }
+} else {
+  $club_id = $_GET["club_id"];
+  $courslist = pull_cours($db, $club_id, $session_seqno, $ss_sql_frag, $courslist);
 }
 
 echo json_encode($courslist);
