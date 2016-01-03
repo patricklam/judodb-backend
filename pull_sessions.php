1c7a4cf952ff58bc90b294b0edc83ea46c1378f4
Patrick Lam <prof.lam@gmail.com>
2016-01-03 11:27:41 +0000

    fix club_id handling

--- pull_sessions.php
+++ pull_sessions.php
@@ -8,7 +8,7 @@ header('content-type: application/json');
 $db = pdo_db_connect();
 require_authentication($db);
 
-$club_id = $_GET["club_id"];
+$club_id = $db->quote($_GET["club_id"]);
 if (!isset($club_id)) die;
 
 if ($club_id == "0") {
@@ -27,7 +27,7 @@ if ($club_id == "0") {
     $sessionlist[] = $session;
   }
   // sessions which do not exist
-  $query = $db->prepare("SELECT `seqno`, `linked_seqno`, `name`, `year`, `abbrev`, -1 AS `id`, 0 AS `club`, '-' AS `first_class_date`, '-' AS `first_signup_date`, '-' AS `last_class_date`, '-' AS `last_signup_date` from `session` WHERE NOT EXISTS (SELECT `session`.`seqno` FROM `session_club` WHERE `session`.`seqno` = `session_club`.`seqno` AND `session_club`.`club` = $club_id);");
+  $query = $db->prepare("SELECT `seqno`, `linked_seqno`, `name`, `year`, `abbrev`, -1 AS `id`, $club_id AS `club`, '-' AS `first_class_date`, '-' AS `first_signup_date`, '-' AS `last_class_date`, '-' AS `last_signup_date` from `session` WHERE NOT EXISTS (SELECT `session`.`seqno` FROM `session_club` WHERE `session`.`seqno` = `session_club`.`seqno` AND `session_club`.`club` = $club_id);");
   $query->execute(array($club_id));
   foreach ($query->fetchAll(PDO::FETCH_OBJ) as $session) {
     $sessionlist[] = $session;
