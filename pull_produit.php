cfdb889235fb1df17ad7e1c1f88868d7c2953a30
Patrick Lam <prof.lam@gmail.com>
2016-08-09 08:44:38 +0000

    always pull produits with club_id=0

--- pull_produit.php
+++ pull_produit.php
@@ -12,7 +12,7 @@ $club_id = $_GET["club_id"];
 if (!isset($club_id) || !can_access_club($db, get_user_id($db), $club_id)) die;
 
 $produitlist = array();
-$produit_query = $db->prepare('SELECT * FROM `produit` WHERE `club_id`=:club_id');
+$produit_query = $db->prepare('SELECT * FROM `produit` WHERE `club_id` = 0 OR `club_id`=:club_id ORDER BY `id`');
 $produit_query->execute(array(':club_id' => $club_id));
 foreach ($produit_query->fetchAll(PDO::FETCH_OBJ) as $produit) {
   $produitlist[] = $produit;
