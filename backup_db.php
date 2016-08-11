5e46cf62a933b24a09225c9a8da07830c8bd0658
Patrick Lam <prof.lam@gmail.com>
2016-08-11 08:43:54 +0000

    create php script and fix dir for backups

---
+++ backup_db.php
@@ -0,0 +1,18 @@
+<?php
+require_once ('_dbconfig.php');
+require_once ('_pdo.php');
+require_once ('_authutils.php');
+require_once ('_userutils.php');
+
+$db = pdo_db_connect();
+require_authentication($db);
+$userid = get_user_id($db);
+if (!is_admin($db, $userid)) die;
+
+$db = NULL;
+
+exec("mysqldump --user=$DBI_USERNAME --password=$DBI_PASSWORD --host=$DBI_HOST $DBI_DATABASE > backups/db-" . date('Ymd-his'));
+
+echo 'backup terminé avec succès: backups/db-' . date('Ymd-his') . '<br>';
+?>
+
